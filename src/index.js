d5c12a66b3ff6e55040627488a87d2284f87a2bf
braianchincho <braianchincho@gmail.com>
2022-08-07 21:09:00 -0300

    Spanish translates is added

--- src/index.js
+++ src/index.js
@@ -7,13 +7,17 @@ import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
 
 import common_en from '../translations/en/common.json';
+import common_es from '../translations/es/common.json';
 
 i18next.init({
   interpolation: { escapeValue: false },
-  lng: 'en',
+  lng: 'es',
   resources: {
     en: {
       common: common_en,
+    },
+    es: {
+      common: common_es,
     },
   },
 });
